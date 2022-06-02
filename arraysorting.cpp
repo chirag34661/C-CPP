d21670f3cd33d6a6ed75f19501e7165c18e31096
Chirag Dewangan <93570903+chirag34661@users.noreply.github.com>
2022-06-02 15:24:53 +0530

    Add files via upload

---
+++ arraysorting.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array: ";
+    cin>>n;
+    int ar[n],temp;
+    cout<<"Enter "<<n<<" elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>ar[i];
+    }
+    for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+            if(ar[i]>ar[j]){
+                temp=ar[i];
+                ar[i]=ar[j];
+                ar[j]=temp;
+           }
+        }
+      }
+    for(int i=0;i<n;i++){
+        cout<<ar[i]<<"\t";
+    }
+system("pause>0");
+}
